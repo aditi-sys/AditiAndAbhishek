8f0acafe0791389f0b7a6a58dbeb2a747fb573c4
Abhishek Tudu <tudu4code@gmail.com>
2021-03-08 15:14:38 +0530

    Update Student.java
    Adding Comments

--- Student.java
+++ Student.java
@@ -1,21 +1,27 @@
 
 public class Student {
 
+	//Declaring variables
 	private String name;
 	private int id;
 	private double percentage;
 	private String[] skills;
 	
+	//Initializing Constructor 1
 	public Student(String name,int id,double percentage,String[] skills) {
 		this.name=name;
 		this.id=id;
 		this.percentage=percentage;
 		this.skills=skills;
 	}
+	//Initializing Constructor 2
 	public Student(String name,int id) {
 		this.name=name;
 		this.id=id;
 	}
+	
+	
+	// Getters and Setters
 	public String getName() {
 		return name;
 	}
